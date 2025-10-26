5f90ae176a8148a51a83304ed59c23838b37076a
Aman Gupta <eagleax7@gmail.com>
2025-10-26 15:40:53 +0530

    validation erro handled and unmarshall error handled

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -1,9 +1,53 @@
 package student
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/amanguptak/students-api/internal/types"
+	"github.com/amanguptak/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
+)
 
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to student api end point"))
+
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+		if err != nil {
+			var ute *json.UnmarshalTypeError
+
+			// Unmarshal Type Error
+
+			// When JSON decoding fails because a field’s type doesn’t match (like a string instead of an int), Go returns an error of this type.
+			if errors.As(err, &ute) {
+				// e.g. "Age should be int, not string"
+				msg := fmt.Sprintf("%s should be %s, not %s", ute.Field, ute.Type.String(), ute.Value)
+				response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("%s", msg)))
+				return
+			}
+
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+	
+
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors) // doing typecasting here
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+			return
+		}
+
+		// w.Write([]byte("Welcome to student api end point"))
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
 	}
 }
